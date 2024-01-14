e96c38cbe601af3b7b637c41720cc07c783d7992
MatiasFochi <matiasfochi.dev@gmail.com>
2024-01-14 02:43:55 -0300

    Actualizacion 14/01/2024

--- src/App.js
+++ src/App.js
@@ -9,13 +9,39 @@ import { evaluate } from 'mathjs';
 function App() {
 
   const [input,setInput] = useState('');
+  const [esNumero,setEsNumero] = useState(true)
+  let ultimoIngresado = '';
 
-  const agregarInput = val => {
+  const agregarNumero = val => {
+    console.log("valor: "+val);
+    setEsNumero(true);
     setInput(input + val);
+    ultimoIngresado = val;
+    console.log("input: "+input);
   };
 
+  const agregarOperador = val => {
+    console.log("valor: "+val);
+    if(esNumero){
+      setInput(input + val);
+      
+    console.log("input: "+input);
+      ultimoIngresado = val;
+    }
+    else{
+      if(val !== ultimoIngresado){
+        let nuevoOperador = input.slice(0,-1);
+        nuevoOperador = nuevoOperador + val;
+        setInput(nuevoOperador);
+        ultimoIngresado = val;
+      }
+    }
+    setEsNumero(false);
+    console.log("Ultimo: " +ultimoIngresado);
+  }
+
   const calcularResultado = () => {
-    if(input){
+    if(input && isNaN(ultimoIngresado)){
       setInput(evaluate(input));
     }
     else{
@@ -34,28 +60,28 @@ function App() {
       <div className='contenedor-calculadora'>
         <Pantalla input={input}/>
         <div className='fila'>
-          <Boton manejarClic = {agregarInput}>1</Boton>
-          <Boton manejarClic = {agregarInput}>2</Boton>
-          <Boton manejarClic = {agregarInput}>3</Boton>
-          <Boton manejarClic = {agregarInput}>+</Boton>
+          <Boton manejarClic = {agregarNumero}>1</Boton>
+          <Boton manejarClic = {agregarNumero}>2</Boton>
+          <Boton manejarClic = {agregarNumero}>3</Boton>
+          <Boton manejarClic = {agregarOperador}>+</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClic = {agregarInput}>4</Boton>
-          <Boton manejarClic = {agregarInput}>5</Boton>
-          <Boton manejarClic = {agregarInput}>6</Boton>
-          <Boton manejarClic = {agregarInput}>-</Boton>
+          <Boton manejarClic = {agregarNumero}>4</Boton>
+          <Boton manejarClic = {agregarNumero}>5</Boton>
+          <Boton manejarClic = {agregarNumero}>6</Boton>
+          <Boton manejarClic = {agregarOperador}>-</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClic = {agregarInput}>7</Boton>
-          <Boton manejarClic = {agregarInput}>8</Boton>
-          <Boton manejarClic = {agregarInput}>9</Boton>
-          <Boton manejarClic = {agregarInput}>*</Boton>
+          <Boton manejarClic = {agregarNumero}>7</Boton>
+          <Boton manejarClic = {agregarNumero}>8</Boton>
+          <Boton manejarClic = {agregarNumero}>9</Boton>
+          <Boton manejarClic = {agregarOperador}>*</Boton>
         </div>
         <div className='fila'>
           <Boton manejarClic = {calcularResultado}>=</Boton>
-          <Boton manejarClic = {agregarInput}>0</Boton>
-          <Boton manejarClic = {agregarInput}>.</Boton>
-          <Boton manejarClic = {agregarInput}>/</Boton>
+          <Boton manejarClic = {agregarNumero}>0</Boton>
+          <Boton manejarClic = {agregarOperador}>.</Boton>
+          <Boton manejarClic = {agregarOperador}>/</Boton>
         </div>
         <div className='fila'>
           <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
